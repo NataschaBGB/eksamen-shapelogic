67e843827de9352fe955dddefebec3aca1f287ed
NataschaBGB <nannabjerning@hotmail.com>
2026-06-15 13:12:36 +0200

    add: employees links and style

--- src/components/Employees/index.jsx
+++ src/components/Employees/index.jsx
@@ -6,20 +6,102 @@ export default function Employees() {
     return (
         <section className="employees">
             <div className="container">
-                <h2>Vores medarbejdere</h2>
+
+                <h2 className="employees__title">Vores medarbejdere</h2>
+                
                 <div className="employees__list">
-                    <div className="employee">
+                    
+                    <div className="employees__employee">
                         <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 1" className="employee-image" />
                         <h3>Medarbejder 1</h3>
                         <p>Position: Softwareudvikler</p>
                     </div>
-                    <div className="employee">
-                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
 
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
                         <h3>Medarbejder 2</h3>
                         <p>Position: Grafisk Designer</p>
                     </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 3</h3>
+                        <p>Position: Frontend Udvikler</p>
                     </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 4</h3>
+                        <p>Position: Frontend Udvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 5</h3>
+                        <p>Position: Grafisk Designer</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 6</h3>
+                        <p>Position: Softwareudvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 7</h3>
+                        <p>Position: Backend Udvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 8</h3>
+                        <p>Position: Frontend Udvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 9</h3>
+                        <p>Position: Softwareudvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 10</h3>
+                        <p>Position: Grafisk Designer</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 11</h3>
+                        <p>Position: Backend Udvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 12</h3>
+                        <p>Position: Frontend Udvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 13</h3>
+                        <p>Position: Softwareudvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 14</h3>
+                        <p>Position: Frontend Udvikler</p>
+                    </div>
+
+                    <div className="employees__employee">
+                        <img src="/eksamen-shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
+                        <h3>Medarbejder 15</h3>
+                        <p>Position: Softwareudvikler</p>
+                    </div>
+                
+                </div>
             </div>
         </section>
     );
