cc72fd9136d99c5af3d803d0d8f34870b740d0f3
NataschaBGB <nannabjerning@hotmail.com>
2026-06-15 12:57:57 +0200

    fix: image paths

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -12,37 +12,37 @@ export default function Products() {
                     
                     <div className="products__item">
                         <h2>Gaming Monitor</h2>
-                        <img src="/shapelogic/monitor.png" alt="Gaming Monitor" className="product-image" />
+                        <img src="/eksamen-shapelogic/monitor.png" alt="Gaming Monitor" className="product-image" />
                         <p>2999 kr.</p>
                     </div>
 
                     <div className="products__item">
                         <h2>Gaming Bundle</h2>
-                        <img src="/shapelogic/gamingbundle.png" alt="Gaming Bundle" className="product-image" />
+                        <img src="/eksamen-shapelogic/gamingbundle.png" alt="Gaming Bundle" className="product-image" />
                         <p>5999 kr.</p>
                     </div>
 
                     <div className="products__item">
                         <h2>Keyboard og Mus</h2>
-                        <img src="/shapelogic/keyboardmouse.jpg" alt="Keyboard og Mus" className="product-image" />
+                        <img src="/eksamen-shapelogic/keyboardmouse.jpg" alt="Keyboard og Mus" className="product-image" />
                         <p>899 kr.</p>
                     </div>
 
                     <div className="products__item">
                         <h2>Gaming Headset</h2>
-                        <img src="/shapelogic/headset.jpg" alt="Gaming Headset" className="product-image" />
+                        <img src="/eksamen-shapelogic/headset.jpg" alt="Gaming Headset" className="product-image" />
                         <p>699 kr.</p>
                     </div>
 
                     <div className="products__item">
                         <h2>Gaming Bundle</h2>
-                        <img src="/shapelogic/gamingbundle.png" alt="Gaming Bundle" className="product-image" />
+                        <img src="/eksamen-shapelogic/gamingbundle.png" alt="Gaming Bundle" className="product-image" />
                         <p>5999 kr.</p>
                     </div>
 
                     <div className="products__item">
                         <h2>Gaming Monitor</h2>
-                        <img src="/shapelogic/monitor.png" alt="Gaming Monitor" className="product-image" />
+                        <img src="/eksamen-shapelogic/monitor.png" alt="Gaming Monitor" className="product-image" />
                         <p>2999 kr.</p>
                     </div>
 
