9de4b70233b54e0dbbec791892e133e0bda77e66
NataschaBGB <nannabjerning@hotmail.com>
2026-06-15 12:43:45 +0200

    fix: employee image

--- src/components/Employees/index.jsx
+++ src/components/Employees/index.jsx
@@ -9,12 +9,12 @@ export default function Employees() {
                 <h2>Vores medarbejdere</h2>
                 <div className="employees__list">
                     <div className="employee">
-                        <img src="/shapelogic/employee1.jpg" alt="Medarbejder 1" className="employee-image" />
+                        <img src="/shapelogic/employee.png" alt="Medarbejder 1" className="employee-image" />
                         <h3>Medarbejder 1</h3>
                         <p>Position: Softwareudvikler</p>
                     </div>
                     <div className="employee">
-                        <img src="/shapelogic/employee2.jpg" alt="Medarbejder 2" className="employee-image" />
+                        <img src="/shapelogic/employee.png" alt="Medarbejder 2" className="employee-image" />
 
                         <h3>Medarbejder 2</h3>
                         <p>Position: Grafisk Designer</p>
